2e390ab9ed9ef8a04da6660b5c7ea2487b48ddf1
Denis Onchukov <cadako@gmail.com>
2025-12-09 14:20:23 +0300

    Add parametrize to tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,34 +10,3 @@ class MainPage:
     def open(self):
         self.page.goto(self.BASE_URL)
 
-    @property
-    def about_us_link(self) -> Locator:
-        return self.page.get_by_role(role='link', name='О нас', exact=True)
-
-    @property
-    def contacts_link(self) -> Locator:
-        return self.page.get_by_role(role='link', name='Контакты', exact=True)
-
-    @property
-    def specializations_link(self) -> Locator:
-        return self.page.get_by_role(role='link', name='Вакансии', exact=True)
-
-    @property
-    def feedback_link(self) -> Locator:
-        return self.page.get_by_role(role='link', name='Отзывы', exact=True)
-
-    @property
-    def about_us_anchor(self) -> Locator:
-        return self.page.get_by_text('О компании')
-
-    @property
-    def contacts_anchor(self) -> Locator:
-        return self.page.get_by_text('Контактная информация')
-
-    @property
-    def specializations_anchor(self) -> Locator:
-        return self.page.get_by_text('Кого мы ищем')
-
-    @property
-    def feedback_anchor(self) -> Locator:
-        return self.page.get_by_text('Отзывы специалистов')
--- tests/test_main_page_navigation.py
+++ tests/test_main_page_navigation.py
@@ -2,42 +2,26 @@
 import allure
 import pytest
 from pages.main_page import MainPage
+from playwright.sync_api import Page, Locator
 
 
 class TestNavigation:
-
-    @allure.title("Проверка перехода к блоку 'О нас'")
-    def test_about_us_link_block(self, page: MainPage):
+    @pytest.mark.parametrize(
+        "link_text, anchor, anchor_text",
+        [('О нас', '#about', 'О компании'),
+         ('Контакты', '#contact', 'Контактная информация'),
+         ('Вакансии', '#specializations', 'Кого мы ищем'),
+         ('Отзывы', '#testimonials', 'Отзывы специалистов')]
+    )
+    @allure.title("Проверка перехода к блоку {link_text}")
+    def test_navigation_to_block(self, page: Page, link_text: str,
+                                 anchor: str, anchor_text: str):
         main_page = MainPage(page)
-        main_page.open()
-        main_page.about_us_link.click()
+        with allure.step(f'Открываем страницу {main_page.BASE_URL}'):
+            main_page.open()
+        with allure.step(f"Кликаем по ссылке '{link_text}'"):
+            page.get_by_role(role='link', name=link_text, exact=True).click()
         with allure.step("Проверяем наличие якоря в URL"):
-            assert "#about" in page.url
-        assert main_page.about_us_anchor.is_visible()
-
-    @allure.title("Проверка перехода к блоку 'Контакты'")
-    def test_contacts_link_block(self, page: MainPage):
-        main_page = MainPage(page)
-        main_page.open()
-        main_page.contacts_link.click()
-        with allure.step("Проверяем наличие якоря в URL"):
-            assert "#contact" in page.url
-        assert main_page.contacts_anchor.is_visible()
-
-    @allure.title("Проверка перехода к блоку 'Вакансии'")
-    def test_specializations_link_block(self, page: MainPage):
-        main_page = MainPage(page)
-        main_page.open()
-        main_page.specializations_link.click()
-        with allure.step("Проверяем наличие якоря в URL"):
-            assert "#specializations" in page.url
-        assert main_page.specializations_anchor.is_visible()
-
-    @allure.title("Проверка перехода к блоку 'Отзывы'")
-    def test_feedback_link_block(self, page: MainPage):
-        main_page = MainPage(page)
-        main_page.open()
-        main_page.feedback_link.click()
-        with allure.step("Проверяем наличие якоря в URL"):
-            assert "#testimonials" in page.url
-        assert main_page.feedback_anchor.is_visible()
+            assert anchor in page.url
+        with allure.step("Проверяем видимость целевого блока"):
+            assert page.get_by_text(anchor_text).is_visible()
